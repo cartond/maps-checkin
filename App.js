82ca908cb20c3a9bebcfb5f2089588341be17040
Dan <cartond93@gmail.com>
2019-07-30 16:50:35 -0400

    Auth screen followup and icons

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { Navigation } from 'react-native-navigation';
 
 import AuthScreen from './src/screens/Auth/Auth';
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
 //Register Screens
 Navigation.registerComponent("maps-checkin.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("maps-checkin.SharePlaceScreen", () => SharePlaceScreen);
+Navigation.registerComponent("maps-checkin.FindPlaceScreen", () => FindPlaceScreen);
 
 //Start navigation app
 Navigation.startSingleScreenApp({
--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'React'
-import {View, Text} from 'react-native'
+import {View, Text, Button} from 'react-native'
+
+import startMainTabs from '../MainTabs/startMainTabs'
 
 class AuthScreen extends Component {
+  loginHandler = () => {
+    startMainTabs();
+  }
+
   render () {
     return (
       <View>
         <Text>Auth Screen Test</Text>
+        <Button title="Login" onPress={this.loginHandler}></Button>
       </View>
     );
   }
